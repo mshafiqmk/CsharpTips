0f12838730c92e9acb5cbe59651fe4fd84bb5003
shafiq khuidad <shafiqkhuidad@gmail.com>
2018-10-25 10:45:33 -0700

    Generating sequence using Enumerable.Range Method()

---
+++ CsharpTips/AutoGenerationSequence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpTips
+{
+    public class AutoGenerationSequence
+    {
+        public static void GenerateSequence(int start , int end)
+        {
+            var sequenceFromStartToEnd = Enumerable.Range(start, end);
+
+            foreach (var item in sequenceFromStartToEnd)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static int[] GenerateSequenceAsArray(int start , int end)
+        {
+            return Enumerable.Range(start, end).ToArray();
+        }
+        public static List<int> GenerateSequenceAsList(int start, int end)
+        {
+            return Enumerable.Range(start, end).ToList();
+        }
+    }
+}
--- CsharpTips/Program.cs
+++ CsharpTips/Program.cs
@@ -15,6 +15,18 @@ namespace CsharpTips
 
             Console.WriteLine("Making tuple of two list using zip() ext method ");
             EnumsWithLinq.MakeTupleOfTwoLists();
+
+            //Generating sequence -> AutoGenerationSequence.cs
+
+            //Generating sequence from 1 to 5
+            AutoGenerationSequence.GenerateSequence(1,5);
+
+            //Generating array from 10 to 60
+            var arraryFromTenToFifty = AutoGenerationSequence.GenerateSequenceAsArray(10, 50);
+
+            //Generating list from 100 to 129 
+            var listFromHunderedToOneThrity = AutoGenerationSequence.GenerateSequenceAsList(100, 30);
+
         }
     }
 }
